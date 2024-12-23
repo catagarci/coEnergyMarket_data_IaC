8d57ccc71248abaeeb714f643b9cdca6ea9b6ab4
catalina.i.garcia <catalina.i.garcia@accenture.com>
2024-12-22 23:17:13 -0500

    ACTUALIZACION

---
+++ dp-glue-curated-tables-energy/s3.tf
@@ -0,0 +1,3 @@
+data "aws_s3_bucket" "existing_glue_bucket" {
+  bucket = "co-energy-marketer-artifacts-569587453910-pdn"
+}
---
+++ dp-glue-curated-tables-energy/s3_temp.tf
@@ -0,0 +1,3 @@
+data "aws_s3_bucket" "existing_glue_temp_bucket" {
+  bucket = "bucket-glue-temp"
+}
